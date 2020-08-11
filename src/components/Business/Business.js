ec0178af6c5da6bff8f13c121b31ac413d4541e4
Fabrice Innocent <fab33150@gmail.com>
2020-08-11 17:01:23 -0400

    Updated css files and new component BusinessList

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './Business.css'
 const business = {
 imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
   name: 'MarginOtto Pizzeria',
@@ -36,3 +36,4 @@ class Business extends React.Component {
         );
     }
 }
+export default Business;
